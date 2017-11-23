3c5b87fa19448f1e3a50109dc521b1653bcbe14b
RSINT\KRAUS_JU <j-kraus@gmx.de>
2017-11-23 14:40:29 +0100

    done 4 today finish footballPrediction

--- svm.py
+++ svm.py
@@ -2,7 +2,10 @@ from sklearn.preprocessing import StandardScaler
 from sklearn import svm
 import pandas as pd
 from sklearn.metrics import confusion_matrix
-import random
+import numpy as np
+import matplotlib.pyplot as plt
+from matplotlib import cm
+import itertools
 
 # still to do: shuffle X_train, y_train
 
@@ -28,7 +31,57 @@ def doSVM():
     print('the score is')
     print(clf.score(X_test, y_test))
     y_pred = clf.predict(X_test)
-    print(confusion_matrix(y_test, y_pred))
+    # Compute confusion matrix
+    cnf_matrix = confusion_matrix(y_test, y_pred)
+    np.set_printoptions(precision=2)
+
+    # Plot non-normalized confusion matrix
+    plt.figure()
+    plot_confusion_matrix(cnf_matrix, classes=['away', 'draw', 'home'],
+                          title='Confusion matrix, without normalization')
+
+    # Plot normalized confusion matrix
+    plt.figure()
+    plot_confusion_matrix(cnf_matrix, classes=['away', 'draw', 'home'], normalize=True,
+                          title='Normalized confusion matrix')
+
+
+    plt.show()
+
+
+def plot_confusion_matrix(cm, classes,
+                          normalize=False,
+                          title='Confusion matrix',
+                          cmap=cm.Blues):
+    """
+    This function prints and plots the confusion matrix.
+    Normalization can be applied by setting `normalize=True`.
+    """
+    if normalize:
+        cm = cm.astype('float') / cm.sum(axis=1)[:, np.newaxis]
+        print("Normalized confusion matrix")
+    else:
+        print('Confusion matrix, without normalization')
+
+    print(cm)
+
+    plt.imshow(cm, interpolation='nearest', cmap=cmap)
+    plt.title(title)
+    plt.colorbar()
+    tick_marks = np.arange(len(classes))
+    plt.xticks(tick_marks, classes, rotation=45)
+    plt.yticks(tick_marks, classes)
+
+    fmt = '.2f' if normalize else 'd'
+    thresh = cm.max() / 2.
+    for i, j in itertools.product(range(cm.shape[0]), range(cm.shape[1])):
+        plt.text(j, i, format(cm[i, j], fmt),
+                 horizontalalignment="center",
+                 color="white" if cm[i, j] > thresh else "black")
+
+    plt.tight_layout()
+    plt.ylabel('True label')
+    plt.xlabel('Predicted label')
 
 
 doSVM()
